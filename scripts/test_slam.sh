77a480cc3043cfbf49413e7ec5bf1564adaaedcf
zaclaptop <zs1234554@gmail.com>
2026-03-15 03:49:22 +0800

    fix script

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Launch turtlebot inside your world
-xterm -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch; bash" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 
 # Launch gmapping SLAM
-xterm -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch; bash" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
 
 # Launch RViz navigation view
-xterm -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch; bash" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 
 # Launch keyboard teleop
-xterm -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch; bash" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
