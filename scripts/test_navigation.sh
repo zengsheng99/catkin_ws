3dfa5a95ba9ed4edf611f691495d5c0bd1404556
zaclaptop <zs1234554@gmail.com>
2026-03-15 17:05:12 +0800

    fixed turtlebot position

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -3,9 +3,10 @@ export TURTLEBOT_3D_SENSOR=kinect
 export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ubuntu/catkin_ws/src/my_robot/worlds/this_world.world
 export TURTLEBOT_GAZEBO_MAP_FILE=/home/ubuntu/catkin_ws/src/my_robot/maps/map.yaml
+export ROBOT_INITIAL_POSE="-x 4 -y -4 -z 0.0"
 
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ubuntu/catkin_ws/src/my_robot/worlds/this_world.world; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ubuntu/catkin_ws/src/my_robot/worlds/this_world.world; export ROBOT_INITIAL_POSE='-x 4 -y -4 -z 0.0'; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=/home/ubuntu/catkin_ws/src/my_robot/maps/map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=/home/ubuntu/catkin_ws/src/my_robot/maps/map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch initial_pose_x:=4 initial_pose_y:=-4" &
 sleep 5
 xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
