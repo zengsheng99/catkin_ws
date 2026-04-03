33d22858647834713224f966cf9b7d12aba33396
zaclaptop <zs1234554@gmail.com>
2026-04-03 16:12:51 +0800

    remove absolute path for scripts

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
+CATKIN_WS=$(cd "$(dirname "$0")/.." && pwd)
 export TURTLEBOT_3D_SENSOR=kinect
-export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ubuntu/catkin_ws/src/my_robot/worlds/this_world.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/ubuntu/catkin_ws/src/my_robot/maps/map.yaml
+export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib
+export TURTLEBOT_GAZEBO_WORLD_FILE=$CATKIN_WS/src/my_robot/worlds/this_world.world
+export TURTLEBOT_GAZEBO_MAP_FILE=$CATKIN_WS/src/my_robot/maps/map.yaml
 export ROBOT_INITIAL_POSE="-x -4 -y -4 -z 0.0"
 
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ubuntu/catkin_ws/src/my_robot/worlds/this_world.world; export ROBOT_INITIAL_POSE='-x -4 -y -4 -z 0.0'; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib; export TURTLEBOT_GAZEBO_WORLD_FILE=$CATKIN_WS/src/my_robot/worlds/this_world.world; export ROBOT_INITIAL_POSE='-x -4 -y -4 -z 0.0'; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=/home/ubuntu/catkin_ws/src/my_robot/maps/map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch initial_pose_x:=-3.993 initial_pose_y:=3.802 initial_pose_a:=-1.5708" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=$CATKIN_WS/src/my_robot/maps/map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch initial_pose_x:=-3.993 initial_pose_y:=3.802 initial_pose_a:=-1.5708" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; rosrun rviz rviz -d /home/ubuntu/catkin_ws/src/add_markers/rviz/home_service.rviz" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; rosrun rviz rviz -d $CATKIN_WS/src/add_markers/rviz/home_service.rviz" &
 sleep 5
 source /opt/ros/noetic/setup.bash
-source /home/ubuntu/catkin_ws/devel/setup.bash
+source $CATKIN_WS/devel/setup.bash
 rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{header: {frame_id: "map"}, pose: {pose: {position: {x: -4, y: 4, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: -0.7140286376412575, w: 0.7001164936124343}}, covariance: [0.25,0,0,0,0,0,0,0.25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.06853892326654787]}}'
 sleep 2
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; rosrun pick_objects pick_objects" &
 sleep 2
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; rosrun add_markers add_markers_service" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; rosrun add_markers add_markers_service" &
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+CATKIN_WS=$(cd "$(dirname "$0")/.." && pwd)
 export TURTLEBOT_3D_SENSOR=kinect
-export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib
+export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib
 
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -hold -e "source /opt/ros/noetic/setup.bash; source /home/ubuntu/catkin_ws/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -hold -e "source /opt/ros/noetic/setup.bash; source $CATKIN_WS/devel/setup.bash; export TURTLEBOT_3D_SENSOR=kinect; export GAZEBO_PLUGIN_PATH=$CATKIN_WS/devel/lib:/opt/ros/noetic/lib; roslaunch turtlebot_teleop keyboard_teleop.launch" &
